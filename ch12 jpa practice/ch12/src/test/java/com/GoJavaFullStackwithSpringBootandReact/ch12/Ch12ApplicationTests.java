4168395513056c3c44a5088ef19f1bc93148feab
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-16 17:17:14 -0800

    initialized jpa practice project using zip file I get from springInitializr

---
+++ ch12 jpa practice/ch12/src/test/java/com/GoJavaFullStackwithSpringBootandReact/ch12/Ch12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GoJavaFullStackwithSpringBootandReact.ch12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
