4a5bbdfc5eac674d58ba102c0bbcadf18c2f13e3
Marcus Lee <marcusslee95@berkeley.edu>
2021-02-17 17:12:20 -0800

    support get a particular Todo request and get all Todos for a particular user requests where data we get back is from actual db. Did this by creating an interface that implements JPARepository which gives it the implementation for all the common database operations.

--- ch5HelloWorld/src/main/java/com/example/ch5HelloWorld/todo/InterfaceThatInteractsWithData.java
+++ ch5HelloWorld/src/main/java/com/example/ch5HelloWorld/todo/InterfaceThatInteractsWithData.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository //this will be used to do db interactions
 public interface InterfaceThatInteractsWithData extends JpaRepository<Todo, Integer>{
 	List<Todo> findByUsername(String username); //JPA will recognize we created this custom method -> guess what it does -> and creates custom implementation for it.
 }
